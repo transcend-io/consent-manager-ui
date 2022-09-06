f35571e96f9bd640a64986d11ad4528adc893795
eligrey <~@eligrey.com>
2022-09-06 03:15:01 -0700

    fix default translations builds

--- src/translations/loader.ts
+++ src/translations/loader.ts
@@ -9,7 +9,7 @@ import { settings } from '../settings';
 import { CONSENT_MANAGER_TRANSLATIONS } from './constants';
 
 export const loadedTranslations: Translations = Object.create(null);
-const translationsLocation = settings.messages ?? './ui';
+const translationsLocation = settings.messages ?? './🌐';
 
 export const getTranslations = async (
   language: LanguageKey = pickDefaultLanguage(),
@@ -17,17 +17,19 @@ export const getTranslations = async (
   if (!CONSENT_MANAGER_TRANSLATIONS.includes(language)) {
     throw new Error(`No translations found for language ${language}`);
   }
+  // eslint-disable-next-line no-return-assign
   return (loadedTranslations[language] ??= await (async () => {
     const response = await fetch(`${translationsLocation}/${language}.json`);
     if (!response.ok) {
       throw new Error(`Failed to load translations for language ${language}`);
     }
-    return await response.json();
+    return response.json();
   })());
 };
 
 export const getAllTranslations = async (
   loadAllTranslations = false,
+  // eslint-disable-next-line require-await
 ): Promise<Translations> =>
   loadAllTranslations
     ? // load unloaded translations
