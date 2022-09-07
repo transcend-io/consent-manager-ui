76b8443e53ae3de26e28fa95a87414b50314468d
Michael Farrell <mike@transcend.io>
2022-09-06 23:01:55 -0700

    Pulls constant from airgap.js-types (#44)
    * New regime constnat
    
    * tsc

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,7 @@ import {
   ConsentManagerConfig,
   ConsentManagerConfigInput,
   ViewState,
+  DEFAULT_VIEW_STATE_BY_PRIVACY_REGIME,
 } from '@transcend-io/airgap.js-types';
 
 // local
@@ -32,21 +33,7 @@ const defaultConfig: ConsentManagerConfig = {
     tablet: '640px',
     desktop: '1024px',
   },
-  initialViewStateByPrivacyRegime: {
-    // California
-    // TODO: https://transcend.height.app/T-17251 - migrate to DoNotSellDisclosure
-    CPRA: ViewState.NoticeAndDoNotSell,
-    // EU
-    GDPR: ViewState.QuickOptions,
-    // Brazil
-    LGPD: ViewState.QuickOptions,
-    // Virginia (unreachable as we don't detect this regime yet)
-    CDPA: ViewState.NoticeAndDoNotSell,
-    // Colorado (unreachable as we don't detect this regime yet)
-    CPA: ViewState.NoticeAndDoNotSell,
-    // Other
-    Unknown: ViewState.Hidden,
-  },
+  initialViewStateByPrivacyRegime: DEFAULT_VIEW_STATE_BY_PRIVACY_REGIME,
   privacyPolicy,
   dismissedViewState,
 };
