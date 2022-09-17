0cf149ced82d2f732b6a5d8cbf9f1b2e982e28bc
bencmbrook <ben@transcend.io>
2022-09-17 11:56:37 -0700

    remove cm container

--- src/playground/Main.tsx
+++ src/playground/Main.tsx
@@ -1,35 +1,14 @@
 import { ViewState } from '@transcend-io/airgap.js-types';
 import { h, JSX } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
 import { Config } from './Config';
 
 /**
  * The playground entrypoint
  */
 export default function Main(): JSX.Element {
-  // const [consentManager, setConsentManager] = useState<HTMLElement | null>(
-  //   null,
-  // );
   const setViewState = (viewState: ViewState): void => {
     window.transcend?.showConsentManager({ viewState });
   };
-
-  useEffect(() => {
-    const pollForConsentUI = setInterval(() => {
-      const elt = document.getElementById('transcend-consent-manager');
-      if (elt) {
-        clearInterval(pollForConsentUI);
-        // setConsentManager(elt);
-        const shadow = elt.shadowRoot;
-        const interiorContainer = shadow.children[0];
-        console.log(interiorContainer);
-        const modal = interiorContainer.querySelectorAll('.modal-container');
-        console.log(modal);
-
-        modal.style.position = 'absolute';
-      }
-    }, 50);
-  }, []);
 
   return (
     <div>
