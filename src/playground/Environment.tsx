c152276c8a7ca90e81a51f62b990f56095fb90f0
bencmbrook <ben@transcend.io>
2022-12-13 13:01:41 -0600

    typesafe

--- src/playground/Environment.tsx
+++ src/playground/Environment.tsx
@@ -6,12 +6,8 @@ import { useState } from 'preact/hooks';
  * Get current privacy signals
  */
 export function getPrivacySignalsFromLocalStorage(): Set<UserPrivacySignal> {
-  let privacySignalsString = localStorage.getItem('getPrivacySignals');
-  if (!privacySignalsString) {
-    localStorage.setItem('getPrivacySignals', JSON.stringify([]));
-    privacySignalsString = localStorage.getItem('getPrivacySignals');
-  }
-  return new Set(JSON.parse(privacySignalsString));
+  const privacySignalsString = localStorage.getItem('getPrivacySignals');
+  return new Set(JSON.parse(privacySignalsString || '[]'));
 }
 
 /**
