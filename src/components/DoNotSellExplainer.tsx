680cff33a45b343a77993ebf403af4c31ec7d2f2
bencmbrook <ben@transcend.io>
2022-12-22 12:41:10 -0600

    Dismiss button - keyboard accessibility

--- src/components/DoNotSellExplainer.tsx
+++ src/components/DoNotSellExplainer.tsx
@@ -49,15 +49,11 @@ export function DoNotSellExplainer({
         type="button"
         aria-label="Close"
         className="do-not-sell-explainer-close"
+        onClick={() => {
+          handleSetViewState('close');
+        }}
       >
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 32 32"
-          onClick={() => {
-            handleSetViewState('close');
-          }}
-        >
+        <svg width="24" height="24" viewBox="0 0 32 32">
           {/* eslint-disable-next-line max-len */}
           <path d="M25.71 24.29a.996.996 0 1 1-1.41 1.41L16 17.41 7.71 25.7a.996.996 0 1 1-1.41-1.41L14.59 16l-8.3-8.29A.996.996 0 1 1 7.7 6.3l8.3 8.29 8.29-8.29a.996.996 0 1 1 1.41 1.41L17.41 16l8.3 8.29z" />
         </svg>
