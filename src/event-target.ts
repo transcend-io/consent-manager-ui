c641840833a4509371308f3e58a04278abcea250
bencmbrook <ben@transcend.io>
2023-07-10 15:02:22 -0700

    lint fixes

--- src/event-target.ts
+++ src/event-target.ts
@@ -2,18 +2,27 @@
  * Extend EventTarget to include the Transcend toStringTag
  */
 export class TranscendEventTarget extends EventTarget {
-  #stringTag: string = 'Transcend';
+  #stringTag = 'Transcend';
 
-  get [Symbol.toStringTag]() {
-    console.log(this.#stringTag);
+  /**
+   * Add Transcend to the string tag
+   *
+   * @returns string
+   */
+  get [Symbol.toStringTag](): string {
     return this.#stringTag;
   }
 
-  // Allow airgap.js code to overwrite [Symbol.toStringTag]
+  /**
+   * Allow airgap.js code to overwrite [Symbol.toStringTag]
+   */
   set [Symbol.toStringTag](updatedStringTag: string) {
     this.#stringTag = updatedStringTag;
   }
 
+  /**
+   * @param args - EventTarget constructor parameters
+   */
   constructor(...args: ConstructorParameters<typeof EventTarget>) {
     super(...args);
   }
