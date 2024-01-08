37db70658c52b06f4100ddf2a98bad7be39bf65d
anotherminh <minhngoc.ln@gmail.com>
2024-01-08 14:58:25 -0500

    bump

--- src/event-target.ts
+++ src/event-target.ts
@@ -2,7 +2,7 @@
 
 export const { userAgent } = navigator as {
   /** user agent information */
-  userAgent: string
+  userAgent: string;
 };
 export const IS_EDGEHTML = userAgent.includes('Edge/');
 export const SAFARI_VERSION =
@@ -32,33 +32,33 @@ export const TranscendEventTarget = Object.freeze(
        * but it results in a much smaller build size and maintains
        * native compatibility with EventTarget.prototype.
        */
-    class {
-      /**
-       * EventTarget constructor shim
-       */
-      constructor() {
-        // This is one of the lightest real-EventTarget-inheriting
-        // structures instantiable in EdgeHTML and Safari <14.
-        // const target = call(nativeCreateDocument, domImplementation, '', '');
-        const target = document.implementation.createDocument('', '');
-        // eslint-disable-next-line no-constructor-return
-        return brand(target, STRING_TAG);
+      class {
+        /**
+         * EventTarget constructor shim
+         */
+        constructor() {
+          // This is one of the lightest real-EventTarget-inheriting
+          // structures instantiable in EdgeHTML and Safari <14.
+          // const target = call(nativeCreateDocument, domImplementation, '', '');
+          const target = document.implementation.createDocument('', '');
+          // eslint-disable-next-line no-constructor-return
+          return brand(target, STRING_TAG);
+        }
       }
-    }
     : /**
        * airgap.js-branded native EventTarget subclass
        */
-    class extends EventTarget {
-      /**
-       * Branding applicator constructor
-       *
-       * @param args - EventTarget constructor arguments
-       */
-      constructor(...args: ConstructorParameters<typeof EventTarget>) {
-        super(...args);
-        brand(this, STRING_TAG);
-      }
-    },
+      class extends EventTarget {
+        /**
+         * Branding applicator constructor
+         *
+         * @param args - EventTarget constructor arguments
+         */
+        constructor(...args: ConstructorParameters<typeof EventTarget>) {
+          super(...args);
+          brand(this, STRING_TAG);
+        }
+      },
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ) as any as typeof EventTarget;
 /* eslint-enable max-classes-per-file */
