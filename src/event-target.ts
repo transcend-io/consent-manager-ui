dca94e14dca789ef35b16d7c79b6786038c8657a
anotherminh <minhngoc.ln@gmail.com>
2024-01-08 14:52:58 -0500

    Fix event target compatibilty in Safari < 14

--- src/event-target.ts
+++ src/event-target.ts
@@ -1,29 +1,64 @@
+/* eslint-disable max-classes-per-file */
+
+export const { userAgent } = navigator as {
+  /** user agent information */
+  userAgent: string
+};
+export const IS_EDGEHTML = userAgent.includes('Edge/');
+export const SAFARI_VERSION =
+  userAgent.includes('Safari/') &&
+  !userAgent.includes('Chrome/') &&
+  userAgent.match(/Version\/(\d+)/)?.[1];
+
+export const brand = <T>(object: T, brand: string): T =>
+  Object.defineProperty(object, Symbol.toStringTag, {
+    value: brand,
+    enumerable: true,
+    configurable: false,
+    writable: false,
+  });
+
+const STRING_TAG = 'Transcend';
+
 /**
- * Extend EventTarget to include the Transcend toStringTag
+ * EventTarget shim
  */
-export class TranscendEventTarget extends EventTarget {
-  #stringTag = 'Transcend';
-
-  /**
-   * Add Transcend to the string tag
-   *
-   * @returns string
-   */
-  get [Symbol.toStringTag](): string {
-    return this.#stringTag;
-  }
-
-  /**
-   * Allow airgap.js code to overwrite [Symbol.toStringTag]
-   */
-  set [Symbol.toStringTag](updatedStringTag: string) {
-    this.#stringTag = updatedStringTag;
-  }
-
-  /**
-   * @param args - EventTarget constructor parameters
-   */
-  constructor(...args: ConstructorParameters<typeof EventTarget>) {
-    super(...args);
-  }
-}
+export const EventTargetShim = Object.freeze(
+  IS_EDGEHTML || (SAFARI_VERSION && +SAFARI_VERSION < 14)
+    ? /**
+       * EventTarget shim for EdgeHTML and Safari <14
+       *
+       * Note that this shim performs worse than a full polyfill,
+       * but it results in a much smaller build size and maintains
+       * native compatibility with EventTarget.prototype.
+       */
+    class {
+      /**
+       * EventTarget constructor shim
+       */
+      constructor() {
+        // This is one of the lightest real-EventTarget-inheriting
+        // structures instantiable in EdgeHTML and Safari <14.
+        // const target = call(nativeCreateDocument, domImplementation, '', '');
+        const target = document.implementation.createDocument('', '');
+        // eslint-disable-next-line no-constructor-return
+        return brand(target, STRING_TAG);
+      }
+    }
+    : /**
+       * airgap.js-branded native EventTarget subclass
+       */
+    class extends EventTarget {
+      /**
+       * Branding applicator constructor
+       *
+       * @param args - EventTarget constructor arguments
+       */
+      constructor(...args: ConstructorParameters<typeof EventTarget>) {
+        super(...args);
+        brand(this, STRING_TAG);
+      }
+    },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+) as any as typeof EventTarget;
+/* eslint-enable max-classes-per-file */
