64d3300b933c2bf54a675cdb784117f63c6d85ee
Cami <50341430+csmccarthy@users.noreply.github.com>
2024-03-08 10:19:39 -0600

    await css injection (#160)
    * await css injection

--- src/css.ts
+++ src/css.ts
@@ -6,17 +6,20 @@ import { createHTMLElement } from './utils/create-html-element';
  * Inject CSS into the application
  *
  * @param stylesheetUrl - The default URL to attempt to load CSS rom
+ * @returns Promise that finishes when the stylesheet has loaded
  */
-export const injectCss = (stylesheetUrl: string): void => {
-  const root = getAppContainer();
-  if (root && stylesheetUrl) {
-    const link = createHTMLElement<HTMLLinkElement>('link');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    link.id = stylesheetUrl;
-    link.href = stylesheetUrl;
-    root.appendChild(link);
-  } else {
-    logger.error(`Failed to inject css into consent manager!`);
-  }
-};
+export const injectCss = (stylesheetUrl: string): Promise<void> =>
+  new Promise((res) => {
+    const root = getAppContainer();
+    if (root && stylesheetUrl) {
+      const link = createHTMLElement<HTMLLinkElement>('link');
+      link.type = 'text/css';
+      link.rel = 'stylesheet';
+      link.id = stylesheetUrl;
+      link.href = stylesheetUrl;
+      link.addEventListener('load', () => res());
+      root.appendChild(link);
+    } else {
+      logger.error(`Failed to inject css into consent manager!`);
+    }
+  });
