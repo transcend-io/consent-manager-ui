7535f8029df0539604074c07a0b8cbe671f1dee5
rodrigo.zembrzuski <rodrigo.zembrzuski@shippo.com>
2024-08-14 09:36:04 -0300

    round 3 - do not sell explainer

--- src/components/DoNotSellExplainer.tsx
+++ src/components/DoNotSellExplainer.tsx
@@ -96,7 +96,6 @@ export function DoNotSellExplainer({
                 ? messages.doNotSellOptedIn
                 : messages.doNotSellOptedOut,
             )}
-            initialFocus
           />
 
           <p
