f0d02d6390ca772c84d3b3209b23e72e81646c38
Kate Kazantseva <67483124+kate-kazantseva@users.noreply.github.com>
2025-11-13 08:36:07 -0800

    Fix webview height (#235)
    * send banner height with event
    
    * bump

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -86,6 +86,8 @@ export function Main({
           initialFocusElement(dialogRef.current);
         }
       }, 0);
+      window.dispatchEvent(new CustomEvent('banner-height', { detail:
+        { height: dialogRef.current.clientHeight } }));
     }
   }, [viewState, dialogRef, config.autofocus]);
 
